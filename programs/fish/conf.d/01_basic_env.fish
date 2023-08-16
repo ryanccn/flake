8b4bb07622763720e2249f16d88aa0cdee8da237
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-08-16 12:11:55 +0800

    fix(fish): patch PATH

--- programs/fish/conf.d/01_basic_env.fish
+++ programs/fish/conf.d/01_basic_env.fish
@@ -1,6 +1,10 @@
 # basic environment variables
 set -x LANG en_US.UTF-8
-set -x EDITOR "hx"
-set -x PAGER "less"
-set -x LESS "-R"
+set -x EDITOR hx
+set -x PAGER less
+set -x LESS -R
 set -x LS_COLORS "$(vivid generate catppuccin-frappe)"
+
+# nix path patches
+fish_add_path /run/current-system/sw/bin
+fish_add_path "/etc/profiles/per-user/$USER/bin"
