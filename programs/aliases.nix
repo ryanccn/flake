f5b594628e04e3ef4c2e2c21aae2f7acc51f7744
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-08-24 20:55:49 +0800

    style(aliases): "flatten" structure

--- programs/aliases.nix
+++ programs/aliases.nix
@@ -1,12 +1,16 @@
 {
   "vim".command = "hx";
   "dig".command = "doggo";
-  "where".command = "which -a";
 
-  "glol" = {
-    command = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'";
-    fishAlias = true;
-  };
+  "g".command = "git";
+  "g".fishAlias = true;
+
+  "j".command = "just";
+  "j".fishAlias = true;
+
+  "glol".command = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'";
+  "glol".fishAlias = true;
+
   "ghrvw".command = "gh repo view --web";
 
   "opr".command = "op run --env-file=.env.1password --";
@@ -16,8 +20,7 @@
 
   "bcpa".command = "brew cleanup --prune=all";
   "puil".command = "pnpm update --interactive --latest";
-  "pip-upgrade-all" = {
-    command = "pip --disable-pip-version-check list --outdated --format=json | python -c \"import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))\" | xargs -n1 pip install -U";
-    fishAlias = true;
-  };
+
+  "pip-upgrade-all".command = "pip --disable-pip-version-check list --outdated --format=json | python -c \"import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))\" | xargs -n1 pip install -U";
+  "pip-upgrade-all".fishAlias = true;
 }
