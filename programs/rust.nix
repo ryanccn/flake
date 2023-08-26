a387054cfe8b55b5275b714de56bd44b275c0134
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-08-26 21:24:52 +0800

    feat(rust): use toml generator

--- programs/rust.nix
+++ programs/rust.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: let
   inherit (pkgs) lib;
+  toml = pkgs.formats.toml {};
 in {
-  home.file.".cargo/config.toml".text = ''
-    [build]
-    rustc-wrapper = "${lib.getExe' pkgs.sccache "sccache"}"
-  '';
+  home.file.".cargo/config.toml".source = toml.generate "config.toml" {
+    build.rustc-wrapper = lib.getExe' pkgs.sccache "sccache";
+  };
 }
