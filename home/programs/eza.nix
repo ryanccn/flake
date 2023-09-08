5ff6b584db82847db5e2a4e450e2a3bc998cb4cd
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-09-08 20:48:55 +0800

    fix(eza): use built-in hm option

--- home/programs/eza.nix
+++ home/programs/eza.nix
@@ -1,16 +1,8 @@
-{pkgs, ...}: let
-  aliases = {
-    eza = "eza --all --icons";
-    ls = "eza";
-    ll = "eza -l";
-    la = "eza -a";
-    lt = "eza --tree";
-    lla = "eza -la";
+_: {
+  programs.eza = {
+    enable = true;
+    enableAliases = true;
+    icons = true;
+    extraOptions = ["--all"];
   };
-in {
-  home.packages = [pkgs.eza];
-
-  programs.bash.shellAliases = aliases;
-  programs.zsh.shellAliases = aliases;
-  programs.fish.shellAliases = aliases;
 }
