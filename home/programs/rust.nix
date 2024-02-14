426718a43507efc93b7214561342b6825adb329c
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-02-14 15:26:06 +0800

    fix(rust): adopt sccache again

--- home/programs/rust.nix
+++ home/programs/rust.nix
@@ -1,15 +1,27 @@
 {
   pkgs,
-  config,
+  lib,
   ...
 }: let
   toml = pkgs.formats.toml {};
 in {
   home.file.".cargo/config.toml".source = toml.generate "config.toml" {
-    build.target-dir = "${config.home.homeDirectory}/.cargo/target";
+    build.rustc-wrapper = "${lib.getExe' pkgs.sccache "sccache"}";
   };
 
   home.packages = with pkgs; [
+    (fenix.combine (
+      with fenix;
+      with stable; [
+        cargo
+        rustc
+        rustfmt
+        clippy
+        rust-src
+        targets.wasm32-unknown-unknown.stable.rust-std
+      ]
+    ))
+
     cargo-bloat
     cargo-cache
     cargo-deny
